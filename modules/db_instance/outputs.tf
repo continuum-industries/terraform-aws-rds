df22e80ab823438b50e8120e0f1c693b96c956fd
Emil-Hansen <emil@continuum.industries>
2021-02-08 15:46:35 +0000

    resolve issue where this_mssql would output default values and break the concat logic. Ensure that db_instance.outputs returns the expected value

--- modules/db_instance/outputs.tf
+++ modules/db_instance/outputs.tf
@@ -1,20 +1,23 @@
 locals {
+  this = local.is_mssql ? aws_db_instance.this_mssql[0] : aws_db_instance.this[0]
+
   enhanced_monitoring_iam_role_name     = element(concat(aws_iam_role.enhanced_monitoring.*.name, [""]), 0)
   enhanced_monitoring_iam_role_arn      = element(concat(aws_iam_role.enhanced_monitoring.*.arn, [""]), 0)
-  this_db_instance_address              = element(concat(aws_db_instance.this_mssql.*.address, aws_db_instance.this.*.address, [""]), 0)
-  this_db_instance_arn                  = element(concat(aws_db_instance.this_mssql.*.arn, aws_db_instance.this.*.arn, [""]), 0)
-  this_db_instance_availability_zone    = element(concat(aws_db_instance.this_mssql.*.availability_zone, aws_db_instance.this.*.availability_zone, [""]), 0)
-  this_db_instance_endpoint             = element(concat(aws_db_instance.this_mssql.*.endpoint, aws_db_instance.this.*.endpoint, [""]), 0)
-  this_db_instance_hosted_zone_id       = element(concat(aws_db_instance.this_mssql.*.hosted_zone_id, aws_db_instance.this.*.hosted_zone_id, [""]), 0)
-  this_db_instance_id                   = element(concat(aws_db_instance.this_mssql.*.id, aws_db_instance.this.*.id, [""]), 0)
-  this_db_instance_resource_id          = element(concat(aws_db_instance.this_mssql.*.resource_id, aws_db_instance.this.*.resource_id, [""]), 0)
-  this_db_instance_status               = element(concat(aws_db_instance.this_mssql.*.status, aws_db_instance.this.*.status, [""]), 0)
-  this_db_instance_name                 = element(concat(aws_db_instance.this_mssql.*.name, aws_db_instance.this.*.name, [""]), 0)
-  this_db_instance_username             = element(concat(aws_db_instance.this_mssql.*.username, aws_db_instance.this.*.username, [""]), 0)
-  this_db_instance_port                 = element(concat(aws_db_instance.this_mssql.*.port, aws_db_instance.this.*.port, [""]), 0)
-  this_db_instance_ca_cert_identifier   = element(concat(aws_db_instance.this_mssql.*.ca_cert_identifier, aws_db_instance.this.*.ca_cert_identifier, [""]), 0)
-  this_db_instance_domain               = element(concat(aws_db_instance.this_mssql.*.domain, [""]), 0)
-  this_db_instance_domain_iam_role_name = element(concat(aws_db_instance.this_mssql.*.domain_iam_role_name, [""]), 0)
+
+  this_db_instance_address              = var.create ? local.this.address : ""
+  this_db_instance_arn                  = var.create ? local.this.arn : ""
+  this_db_instance_availability_zone    = var.create ? local.this.availability_zone: ""
+  this_db_instance_endpoint             = var.create ? local.this.endpoint : ""
+  this_db_instance_hosted_zone_id       = var.create ? local.this.hosted_zone_id : ""
+  this_db_instance_id                   = var.create ? local.this.id : ""
+  this_db_instance_resource_id          = var.create ? local.this.resource_id : ""
+  this_db_instance_status               = var.create ? local.this.status : ""
+  this_db_instance_name                 = var.create ? local.this.name : ""
+  this_db_instance_username             = var.create ? local.this.username : ""
+  this_db_instance_port                 = var.create ? local.this.port : ""
+  this_db_instance_ca_cert_identifier   = var.create ? local.this.ca_cert_identifier : ""
+  this_db_instance_domain               = var.create ? local.this.domain : ""
+  this_db_instance_domain_iam_role_name = var.create ? local.this.domain_iam_role_name : ""
 }
 
 output "enhanced_monitoring_iam_role_name" {
